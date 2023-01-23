9c1c969a49abdf7e52e90e0c213fcc3a5346a996
PHarryP <paul.harry.theuma-sielinou@stu.ocsb.ca>
2023-01-23 13:32:29 -0500

    almost

--- src/Repetition.java
+++ src/Repetition.java
@@ -3,10 +3,26 @@ import java.util.Scanner;
 
 public class Repetition {
 	public static void Run() {
-		Scanner in = new Scanner(System.in);
-		Random rnd=new Random(10);
-		
-		
+		//Scanner in = new Scanner(System.in);
+//		Random rnd=new Random();
+//		int upperbound = 10;
+		//int[] array = new int[3];
+		//for (int i = 0;i<array.length;i++) {
+		//	int  int_ar = rnd.nextInt(upperbound);
+		//	array[i] = rnd.nextInt(upperbound);
+		//}
+//		int[] array = new int[rnd.nextInt(upperbound)];
+//		System.out.println(array);
+//		int  int_ar = rnd.nextInt(upperbound);
+//		System.out.println(int_ar);
+
+		int[] ar = new int[3];
+		Random rnd = new Random();
+		int rndnum = rnd.nextInt(10);
+		for(int i = 0; i<10; i++) {
+			 rndnum = rnd.nextInt(ar.length);
+		}
+		System.out.println(rndnum);
 	}
 
 }
--- src/Subprograms.java
+++ src/Subprograms.java
@@ -3,13 +3,25 @@ import java.util.Scanner;
 public class Subprograms {
 	public static void Run() {
 		Scanner in = new Scanner(System.in);
+		StringBuilder res = new StringBuilder();
 		System.out.println("Enter a string");
 		String a = in.nextLine();
-		in.nextLine();
 		System.out.println("Enter a second string");
 		String b = in.nextLine();
 		
+		//String str = "";
 		
+			
+		
+	//	for (int i=0; i<a.length() || i<b.length(); i++) {
+	//		if (i<a.length()) {
+	//			res.append(a.charAt(i));
+	//		}else if(i<b.length()) {
+	//			res.append(b.charAt(i));
+	//		}
+	//	}
+	//	System.out.println(res.toString());
+		 
 	}
 
 }
